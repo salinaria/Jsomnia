202997b367c5e9417bb730baafb910afa499f53a
gaboorstudio <gaboorstudio@gamil.com>
2020-05-03 13:54:33 +0430

    Develop fieldes

--- src/Request.java
+++ src/Request.java
@@ -1,2 +1,23 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Request {
+    private String name;
+    private String url;
+    private String urlType;
+    private HashMap<String,String>headers=new HashMap<>();
+    private String body;
+
+    public Request(String name, String urlType) {
+        this.name = name;
+        this.urlType = urlType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUrlType() {
+        return urlType;
+    }
 }
